c7b66d5f89f89a6831e62f9f279ee1391c00ff33
kirillkarpakov <karpakovkirill@gmail.com>
2024-03-01 04:54:43 +0400

    any experiments

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+};
