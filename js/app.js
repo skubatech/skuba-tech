b45942abaa2ba9f02267bae87e9eaa364c626df0
kirillkarpakov <karpakovkirill@gmail.com>
2024-03-06 17:23:24 +0400

    fix work block

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const approachEL = document.getElementById("approach");
 const approachLink = document.getElementById("approachLink");
 
+const approachSteps = document.querySelectorAll('.approach_step')
+const approachStep = document.querySelector('.approach_steps')
 const projectsEL = document.getElementById("projects");
 const projectsLink = document.getElementById("projectsLink");
 
